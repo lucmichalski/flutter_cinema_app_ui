ac861fd9f4f9b7d1f3048f54e57e0f6880ad9b79
Mikayil S <sert.mikayil@gmail.com>
2020-03-25 00:48:28 +0100

    feat: add app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'components/BottomNavigation/BottomNavigation.dart';
 
@@ -27,6 +28,29 @@ class _HomeScreen extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: Container(
+          margin: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Color(0xFF0C0C0C),
+            borderRadius: BorderRadius.all(Radius.circular(5.0))
+          ),
+          child: IconButton(icon: SvgPicture.asset("assets/icons/bell-outline.svg", color: Colors.white,), onPressed: null)
+        ),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF0C0C0C),
+              borderRadius: BorderRadius.all(Radius.circular(5.0))
+            ),
+            child: IconButton(icon: SvgPicture.asset("assets/icons/settings-outline.svg", color: Colors.white,), onPressed: null)
+          ),
+        ],
+      ),
       body: Container(child: Text("Hello world", style: TextStyle(color: Colors.white),)),
       bottomNavigationBar: BottomNavigation(),
     );
