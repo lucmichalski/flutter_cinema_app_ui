fc011a3e0501fa88e3c4e346dc258200375c2eee
Mikayil S <sert.mikayil@gmail.com>
2020-03-25 23:21:42 +0100

    feat: add hero animation

--- lib/components/MovieCard/MovieCard.dart
+++ lib/components/MovieCard/MovieCard.dart
@@ -15,38 +15,44 @@ class MovieCard extends StatefulWidget {
 class _MovieCard extends State<MovieCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.symmetric(horizontal: 5.0),
-      decoration: BoxDecoration(
-        color: Color(0xFF0C0C0C),
-        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-        image: DecorationImage(
-          image: NetworkImage(widget.movie.cover), 
-          fit: BoxFit.cover
-        ),
-      ),
-      child: InkWell(
-        onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => MovieDetailsScreen(movie: widget.movie))),
+    return Hero(
+      tag: widget.movie.name,
+      child: Material(
+        color: Colors.transparent,
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 23.0),
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
           decoration: BoxDecoration(
+            color: Color(0xFF0C0C0C),
             borderRadius: BorderRadius.all(Radius.circular(20.0)),
-            gradient: LinearGradient(colors: [
-              Colors.black.withOpacity(0.5),
-              Colors.transparent
-            ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+            image: DecorationImage(
+              image: NetworkImage(widget.movie.cover), 
+              fit: BoxFit.cover
+            ),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Text(widget.movie.name, style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),),
-              SizedBox(height: 10.0,),
-              Text(widget.movie.shortDescription, style: TextStyle(fontSize: 11.0, color: Colors.white, fontWeight: FontWeight.w300, height: 2.0),)
-            ],
-          ),
-        ),
+          child: InkWell(
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => MovieDetailsScreen(movie: widget.movie))),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 23.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                gradient: LinearGradient(colors: [
+                  Colors.black.withOpacity(0.5),
+                  Colors.transparent
+                ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(widget.movie.name, style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 10.0,),
+                  Text(widget.movie.shortDescription, style: TextStyle(fontSize: 11.0, color: Colors.white, fontWeight: FontWeight.w300, height: 2.0),)
+                ],
+              ),
+            ),
+          )
+        )
       )
     );
   }
--- lib/screens/MovieDetailsScreen/MovieDetailsScreen.dart
+++ lib/screens/MovieDetailsScreen/MovieDetailsScreen.dart
@@ -19,75 +19,81 @@ class _MovieDetailsScreen extends State<MovieDetailsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: <Widget>[
-          // Screen body
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(image: DecorationImage(image: NetworkImage(widget.movie.cover), fit: BoxFit.cover)),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Colors.black.withOpacity(0.4),
-                  Colors.transparent
-                ], begin: Alignment.bottomCenter, end: Alignment.topCenter)
+      body: Hero(
+        tag: widget.movie.name,
+        child: Material(
+          color: Colors.transparent,
+          child: Stack(
+            children: <Widget>[
+              // Screen body
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(image: DecorationImage(image: NetworkImage(widget.movie.cover), fit: BoxFit.cover)),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      Colors.black.withOpacity(0.4),
+                      Colors.transparent
+                    ], begin: Alignment.bottomCenter, end: Alignment.topCenter)
+                  ),
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 75.0),
+                    padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 24.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(widget.movie.name.toString(), style: TextStyle(color: Colors.white, fontSize: 22.0, fontWeight: FontWeight.bold)),
+                        SizedBox(height: 10.0,),
+                        Text(widget.movie.description.toString(), style: TextStyle(color: Colors.white, fontSize: 11.0, height: 2.0)),
+                        SizedBox(height: 20.0),
+                        Button(text: "Buy tickets"),
+                      ]
+                    ),
+                  ),
+                )
               ),
-              child: Container(
-                margin: EdgeInsets.only(bottom: 75.0),
-                padding: EdgeInsets.symmetric(horizontal: 45.0, vertical: 24.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
+              // Bottom navigation
+              Positioned(
+                left: 0.0,
+                right: 0.0,
+                bottom: 0.0,
+                child: BottomNavigation()
+              ),
+              // Custom AppBar
+              Container(
+                margin: EdgeInsets.only(top: 50.0),
+                padding: EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(widget.movie.name.toString(), style: TextStyle(color: Colors.white, fontSize: 22.0, fontWeight: FontWeight.bold)),
-                    SizedBox(height: 10.0,),
-                    Text(widget.movie.description.toString(), style: TextStyle(color: Colors.white, fontSize: 11.0, height: 2.0)),
-                    SizedBox(height: 20.0),
-                    Button(text: "Buy tickets"),
+                    Container(
+                      width: 35.0,
+                      height: 35.0,
+                      margin: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF0C0C0C).withOpacity(0.7),
+                        borderRadius: BorderRadius.all(Radius.circular(5.0))
+                      ),
+                      child: IconButton(icon: SvgPicture.asset("assets/icons/chevron-left-outline.svg", color: Colors.white,), onPressed: () => Navigator.pop(context))
+                    ),
+                    Container(
+                      width: 35.0,
+                      height: 35.0,
+                      margin: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF0C0C0C).withOpacity(0.7),
+                        borderRadius: BorderRadius.all(Radius.circular(5.0))
+                      ),
+                      child: IconButton(icon: SvgPicture.asset("assets/icons/more-vertical-outline.svg", color: Colors.white,), onPressed: null)
+                    ),
                   ]
-                ),
-              ),
-            )
+                )
+              )
+            ]
           ),
-          // Bottom navigation
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.0,
-            child: BottomNavigation()
-          ),
-          // Custom AppBar
-          Container(
-            margin: EdgeInsets.only(top: 50.0),
-            padding: EdgeInsets.symmetric(horizontal: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: 35.0,
-                  height: 35.0,
-                  margin: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF0C0C0C).withOpacity(0.7),
-                    borderRadius: BorderRadius.all(Radius.circular(5.0))
-                  ),
-                  child: IconButton(icon: SvgPicture.asset("assets/icons/chevron-left-outline.svg", color: Colors.white,), onPressed: () => Navigator.pop(context))
-                ),
-                Container(
-                  width: 35.0,
-                  height: 35.0,
-                  margin: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF0C0C0C).withOpacity(0.7),
-                    borderRadius: BorderRadius.all(Radius.circular(5.0))
-                  ),
-                  child: IconButton(icon: SvgPicture.asset("assets/icons/more-vertical-outline.svg", color: Colors.white,), onPressed: null)
-                ),
-              ]
-            )
-          )
-        ]
+        ),
       ),
     );
   }
