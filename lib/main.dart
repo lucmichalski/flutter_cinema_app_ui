66d69e8a1c0a3ecd33751437a0d204f1af6eb511
Mikayil S <sert.mikayil@gmail.com>
2020-03-25 21:32:11 +0100

    feat: set device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:cinema_app_flutter/screens/HomeScreen/HomeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
